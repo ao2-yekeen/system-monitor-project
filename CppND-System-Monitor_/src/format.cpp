ee3b05320507cf5ca66a6537cea2fe85b0f229c5
ao2-yekeen <69585981+ao2-yekeen@users.noreply.github.com>
2021-06-30 23:01:07 +0100

    Add files via upload

---
+++ CppND-System-Monitor_/src/format.cpp
@@ -0,0 +1,28 @@
+#include <string>
+
+#include "format.h"
+
+using namespace std;
+
+// TODO: Complete this helper function
+// INPUT: Long int measuring seconds
+// OUTPUT: HH:MM:SS
+string Format::ElapsedTime(long int seconds) {
+  long int minutes = 0;
+  long int hours = 0;
+  
+  std::string output = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+  
+  while (seconds >= 60){
+    seconds -= 60;
+    minutes +=1;
+    output = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+  while (minutes >=60){
+     minutes -=60;
+     hours+=1;
+     output = to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+     }
+  }
+  
+  return output;
+}

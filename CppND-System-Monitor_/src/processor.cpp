a458d95e3e12a7606874c867499de58777236ee1
ao2-yekeen <69585981+ao2-yekeen@users.noreply.github.com>
2021-07-01 03:25:33 +0100

    Delete CppND-System-Monitor_ directory

--- CppND-System-Monitor_/src/processor.cpp
+++
@@ -1,9 +0,0 @@
-#include "processor.h"
-#include "linux_parser.h"
-#include <vector>
-using std::vector;
-
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { 
- return static_cast<float>(LinuxParser::Jiffies()) / static_cast<float>(LinuxParser::ActiveJiffies())/100;
-}
